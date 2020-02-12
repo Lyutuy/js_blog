b04750b9d767d6d1c82937e208cb04c65c18f48c
Lyutuy <lytuy03115@gmail.com>
2020-02-12 16:24:09 +0200

    add active click

--- src/components/navigation.component.js
+++ src/components/navigation.component.js
@@ -1,9 +1,21 @@
 import { Component } from '../core/component';
 
 export class NavigationComponent extends Component {
-    constructor() {
+    constructor(id) {
         super(id);
     }
 
-    init() {}
+    init() {
+        this.$elem.addEventListener('click', tabClickHandler.bind(this))
+    }
+}
+
+function tabClickHandler(event) {
+    event.preventDefault();
+    if (event.target.classList.contains('tab')) {
+        Array.from(this.$elem.querySelectorAll('.tab')).forEach(tab => {
+            tab.classList.remove('active');
+        });
+        event.target.classList.add('active');
+    }
 }
--- src/core/component.js
+++ src/core/component.js
@@ -1,16 +1,16 @@
 export class Component {
     constructor(id) {
-        this.$el = document.getElementById(id);
+        this.$elem = document.getElementById(id);
         this.init();
     }
 
     init() {}
 
     hide() {
-        this.$el.classList.add('hide');
+        this.$elem.classList.add('hide');
     }
 
     show() {
-        this.$el.classList.remove('');
+        this.$elem.classList.remove('');
     }
 }

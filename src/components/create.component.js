a37b7522338e0373b65024f38ce97690930e2412
Lyutuy <lytuy03115@gmail.com>
2020-02-17 16:58:35 +0200

    add form

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -1,7 +1,27 @@
+import { Form } from '../core/form';
 import { Component } from './../core/component';
 
 export class CreateComponent extends Component {
     constructor(id) {
         super(id);
     }
+
+    init() {
+        this.$el.addEventListener('submit', submitHandler.bind(this));
+
+        this.form = new Form(this.$el, {
+            title: [],
+            fulltext: []
+        });
+    }
+}
+
+function submitHandler(event) {
+    event.preventDefault();
+
+    const formData = {
+        type: this.$el.type.value,
+        ...this.form.value()
+    }
+    console.log('Submit', formData);
 }
